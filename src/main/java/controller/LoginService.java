9532976f3c8570a889a933784204815130cfb52a
reinhold <reinhold6@schule.bremen.de>
2023-06-14 10:14:25 +0200

    USERS SPALTE ANGELEGT

---
+++ src/main/java/controller/LoginService.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class LoginService {
+    public void login(){
+    String password = "PasswordBox.Value";
+    String username = "Username.Value";
+
+
+    }
+}
---
+++ src/main/java/datastorage/LoginDAO.java
@@ -0,0 +1,31 @@
+package datastorage;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoginDAO implements DAO {
+    @Override
+    public void create(Object o) throws SQLException {
+
+    }
+
+    @Override
+    public Object read(long key) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List readAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(Object o) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteById(long key) throws SQLException {
+
+    }
+}
